3654eaf617d6a87d7440076fa041536898505875
Soumyadeep <soumyadeep9@gmail.com>
2014-12-10 16:12:21 +0530

    Linux file added

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Competitive-Programming
 Repository to store Competitive Programming programs and algorithms implemented as part of them.
 
 compProg - > Visual Studio Project for competitive programming.
+cc.cpp	 -> Linux File for competitive programming.
---
+++ cc.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <limits>
+#include <bitset>
+#include <time.h>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+#define MOD 1000000007LL
+#define LL long long
+#define LD long double
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define ABS(x) ((x)<0?-(x):(x))
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define FORR(i,n) for(int i=(n);i>=0;i--)
+
+const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+#define SIZE 100001
+
+int main()
+{
+int n;
+
+
+
+
+	return 0;
+}
