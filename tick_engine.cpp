56c0eb1be2f43a1272ac7d4bbad486063de1d2be
dementor <soumyadeep9@gmail.com>
2015-09-26 20:04:45 +0530

    Bug in Sum

--- tick_engine.cpp
+++ tick_engine.cpp
@@ -10,6 +10,7 @@
 #include <set>
 #include <iostream>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -26,129 +27,7 @@ using namespace std;
 
 const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
 
-// A utility function to get the middle index from corner indexes.
-int getMid(int s, int e) {  return s + (e -s)/2;  }
- 
-/*  A recursive function to get the sum of values in given range of the array.
-    The following are parameters for this function.
- 
-    st    --> Pointer to segment tree
-    si    --> Index of current node in the segment tree. Initially 0 is
-             passed as root is always at index 0
-    ss & se  --> Starting and ending indexes of the segment represented by
-                 current node, i.e., st[si]
-    qs & qe  --> Starting and ending indexes of query range */
-int getSumUtil(int *st, int ss, int se, int qs, int qe, int si)
-{
-    // If segment of this node is a part of given range, then return the
-    // sum of the segment
-    if (qs <= ss && qe >= se)
-        return st[si];
- 
-    // If segment of this node is outside the given range
-    if (se < qs || ss > qe)
-        return 0;
- 
-    // If a part of this segment overlaps with the given range
-    int mid = getMid(ss, se);
-    return getSumUtil(st, ss, mid, qs, qe, 2*si+1) +
-           getSumUtil(st, mid+1, se, qs, qe, 2*si+2);
-}
- 
-/* A recursive function to update the nodes which have the given index in
-   their range. The following are parameters
-    st, si, ss and se are same as getSumUtil()
-    i    --> index of the element to be updated. This index is in input array.
-   diff --> Value to be added to all nodes which have i in range */
-void updateValueUtil(int *st, int ss, int se, int i, int diff, int si)
-{
-    // Base Case: If the input index lies outside the range of this segment
-    if (i < ss || i > se)
-        return;
- 
-    // If the input index is in range of this node, then update the value
-    // of the node and its children
-    st[si] = st[si] + diff;
-    if (se != ss)
-    {
-        int mid = getMid(ss, se);
-        updateValueUtil(st, ss, mid, i, diff, 2*si + 1);
-        updateValueUtil(st, mid+1, se, i, diff, 2*si + 2);
-    }
-}
- 
-// The function to update a value in input array and segment tree.
-// It uses updateValueUtil() to update the value in segment tree
-void updateValue(int arr[], int *st, int n, int i, int new_val)
-{
-    // Check for erroneous input index
-    if (i < 0 || i > n-1)
-    {
-        printf("Invalid Input");
-        return;
-    }
- 
-    // Get the difference between new value and old value
-    int diff = new_val - arr[i];
- 
-    // Update the value in array
-    arr[i] = new_val;
- 
-    // Update the values of nodes in segment tree
-    updateValueUtil(st, 0, n-1, i, diff, 0);
-}
- 
-// Return sum of elements in range from index qs (quey start) to
-// qe (query end).  It mainly uses getSumUtil()
-int getSum(int *st, int n, int qs, int qe)
-{
-    // Check for erroneous input values
-    if (qs < 0 || qe > n-1 || qs > qe)
-    {
-        printf("Invalid Input");
-        return -1;
-    }
- 
-    return getSumUtil(st, 0, n-1, qs, qe, 0);
-}
- 
-// A recursive function that constructs Segment Tree for array[ss..se].
-// si is index of current node in segment tree st
-int constructSTUtil(int arr[], int ss, int se, int *st, int si)
-{
-    // If there is one element in array, store it in current node of
-    // segment tree and return
-    if (ss == se)
-    {
-        st[si] = arr[ss];
-        return arr[ss];
-    }
- 
-    // If there are more than one elements, then recur for left and
-    // right subtrees and store the sum of values in this node
-    int mid = getMid(ss, se);
-    st[si] =  constructSTUtil(arr, ss, mid, st, si*2+1) +
-              constructSTUtil(arr, mid+1, se, st, si*2+2);
-    return st[si];
-}
- 
-/* Function to construct segment tree from given array. This function
-   allocates memory for segment tree and calls constructSTUtil() to
-   fill the allocated memory */
-int *constructST(int arr[], int n)
-{
-    // Allocate memory for segment tree
-    int x = (int)(ceil(log2(n))); //Height of segment tree
-    int max_size = 2*(int)pow(2, x) - 1; //Maximum size of segment tree
-    int *st = new int[max_size];
- 
-    // Fill the allocated memory st
-    constructSTUtil(arr, 0, n-1, st, 0);
- 
-    // Return the constructed segment tree
-    return st;
-}
- 
+
 
 struct tick
 {
@@ -177,8 +56,12 @@ void Tokenize(const string& str,vector<string>& tokens,const string& delimiters 
 }
 
 vector<tick> tickset;
+map <int,int> indexMap;
+map <string, map<string,int> > symbolField;
+map <string, map<string,int> > sum_symbolField;
+vector <  map <string, map<string,int> > > time_sum; 
 
-void readData()
+int readData()
 {
    	string s;
 	vector<string> first_tokens;
@@ -186,7 +69,7 @@ void readData()
 	Tokenize(s,first_tokens);
     string::size_type sz;
 	int ticklines = std::stoi (first_tokens[1],&sz);
-	cout<<ticklines<<endl;
+//	cout<<ticklines<<endl;
 	REP(i,ticklines)
 	{
 		tick dat;
@@ -194,17 +77,38 @@ void readData()
 		getline(std::cin,s);
 		Tokenize(s,data_tokens);
 		dat.timestamp=stoi(data_tokens[0],&sz);
-		dat.symbol = data_tokens[1];
-
-		FOR(j,2,data_tokens.size())
+		string symbol = data_tokens[1];
+		dat.symbol = symbol;
+		if(i>0 && tickset[i-1].timestamp==dat.timestamp);
+		else indexMap[dat.timestamp]=i;
+	    for(int j=2;j<data_tokens.size();j=j+2)
 		{
-			if(j%2==0)
-				dat.field_name.push_back(data_tokens[j]);
-			else dat.field_value.push_back(stoi(data_tokens[j],&sz));
+			string name;
+			int value;
+			name = data_tokens[j];
+			dat.field_name.push_back(name);
+			value = stoi(data_tokens[j+1],&sz);
+			dat.field_value.push_back(value);
+			symbolField[symbol][name]=value;
+			sum_symbolField[symbol][name]+=value;
+//			time_sum.push_back(sum_symbolField);
 		}
+		/*if(indexMap[dat.timestamp]!=i)
+		{
+			//cout<<"Here"<<i<<endl;
+			time_sum[indexMap[dat.timestamp]]=sum_symbolField;
+		}
+		else
+		{*/
+			time_sum.push_back(sum_symbolField);
+		
 		tickset.push_back(dat);
 	}
+	sum_symbolField.clear();
+	cout<<"tickfile completed"<<endl;
+	return ticklines;
 }
+
 
 void printData()
 {
@@ -228,11 +132,12 @@ int findMax(int start_time,int end_time,string symbol,string field_name,int k)
 	return max;
 }
 
-void findSum(int start_time,int end_time,string symbol,string field_name)
+int findSum(int start_time,int end_time,string symbol,string field_name)
 {
-
-
-	
+	int index1 = indexMap[start_time];
+	int index2 = indexMap[end_time];
+	int sum = time_sum[index2][symbol][field_name]-time_sum[index1][symbol][field_name];
+	return sum;
 }
 
 void findSOP(int start_time,int end_time,string symbol,string field_name_one,string field_name_two)
@@ -248,7 +153,7 @@ void readQuery()
 	{
 		string s;
 		vector<string> query_tokens;
-		std::getline (std::cin,s);
+		getline (std::cin,s);
 		Tokenize(s,query_tokens);
 		string::size_type sz;
 		int start_time=stoi(query_tokens[1],&sz);
@@ -267,21 +172,24 @@ void readQuery()
 		{
 		case 0: cout<<findMax(start_time,end_time,symbol,query_tokens[4],stoi(query_tokens[5],&sz));
 			break;
-		case 1: //cout<<findSum(start_time,end_time,symbol,query_tokens[4]);
+		case 1: cout<<findSum(start_time,end_time,symbol,query_tokens[4])<<endl;
 			break;
 		case 2: //cout<<findSOP(start_time,end_time,symbol,query_tokens[4],query_tokens[5]);
 			break;
 			
 		}
-	}
-	
+	}	
+
 }
+
 int main()
 {
 	readData();
-	printData();
+	//printData();
 	readQuery();
+	
+	//cout<<symbolField["s1"]["f3"];
 
-
+	
 	return 0;
 }
